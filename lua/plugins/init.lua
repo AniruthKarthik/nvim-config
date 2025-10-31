53ef78f0824e3bfd4598697df8f8fea69de655c6
Ani <aniruthkarthik10@gmail.com>
2025-10-31 12:45:23 +0530

    added fn functions

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,48 @@
 return {
   {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    dependencies = {
+      "windwp/nvim-ts-autotag",
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "vim", "lua", "vimdoc",
+          "html", "css", "javascript", "typescript",
+          "python", "java", "cpp", "c", "rust", "go"
+        },
+        highlight = {
+          enable = true,
+        },
+        indent = {
+          enable = true,
+        },
+        autotag = {
+          enable = true,
+        },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ["am"] = {
+                query = "@function.outer",
+                desc = "Select around a method/function definition",
+              },
+              ["im"] = {
+                query = "@function.inner",
+                desc = "Select inside a method/function definition",
+              },
+            },
+          },
+        },
+      })
+    end,
+  },
+  {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
